536db37331f2aa92279550aa2a78dcc34604e73d
fiorda04 <santi.fiorda04@gmail.com>
2021-05-26 15:28:37 -0300

    usuario controller incompleto
    se avanzo con el usuario controller pero no se pudo llegar a correr correctamente, sale un error y no puedo solucionarlo. (name no puede ser null)

---
+++ Grupo-14-OO2-2021/src/main/java/com/unla/Grupo14OO22021/controllers/UsuarioController.java
@@ -0,0 +1,38 @@
+package com.unla.Grupo14OO22021.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.unla.Grupo14OO22021.models.UsuarioModel;
+import com.unla.Grupo14OO22021.services.IUsuarioService;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	
+	@Autowired
+	@Qualifier("usuarioService")//decia usuario controller
+	private IUsuarioService usuarioService;
+
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView mAV = new ModelAndView("usuario/index");
+		mAV.addObject("usuarios", usuarioService.getAll());
+		mAV.addObject("usuario", new UsuarioModel());
+		return mAV;
+	}
+
+	@PostMapping("/")
+
+	public RedirectView create (@ModelAttribute("usuario") UsuarioModel usuarioModel) {
+		usuarioService.insertOrUpdate(usuarioModel);
+		return new RedirectView("home/index");
+	}
+}
